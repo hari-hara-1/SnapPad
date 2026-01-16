540c49de71b5f95bc447b959e14c3475c73b084e
hari-hara-1 <hari.shenbagan@gmail.com>
2026-01-16 09:31:07 +0530

    First commit
    Created a project, filled .gitignore file

---
+++ src/test/java/com/webtech/snappad/SnapPadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webtech.snappad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnapPadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
