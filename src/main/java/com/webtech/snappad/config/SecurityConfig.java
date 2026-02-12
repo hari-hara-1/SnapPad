fb6c85d24e18a9ecef32a00772d3b620dfa65c0e
hari-hara-1 <hari.shenbagan@gmail.com>
2026-02-12 12:48:57 +0530

    Added frontend to setAllowedOrigins

--- src/main/java/com/webtech/snappad/config/SecurityConfig.java
+++ src/main/java/com/webtech/snappad/config/SecurityConfig.java
@@ -63,7 +63,7 @@ public class SecurityConfig {
 
         config.setAllowedOrigins(List.of(
                 "http://localhost:5173",
-                "http://localhost:3000"
+                "https://snappad-app.vercel.app"
         ));
         config.setAllowedMethods(List.of(
                 "GET", "POST", "PUT", "DELETE", "OPTIONS"
